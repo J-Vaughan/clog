5d16b50ebd53232c99de35871387c9c8e55bf53c
James Vaughan <dev.jamesvaughan@gmail.com>
2017-11-20 23:01:32 -0800

    Simplified NASA-grade prologs
    This isn't rocket science ;)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /* LICENSE AND CONTACT INFORMATION * * * * * * * * * * * * * * * * * * * * * * *
  * CLog, a logging tool written in C                                           *
- * Copyright (C) 2017 James Vaughan                                            *
+ * Copyright (C) 2017 James Vaughan <dev.jamesvaughan@gmail.com>               *
  *                                                                             *
  * This program is free software: you can redistribute it and/or modify        *
  * it under the terms of the GNU General Public License as published by        *
@@ -14,8 +14,6 @@
  *                                                                             *
  * You should have received a copy of the GNU General Public License           *
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
- *                                                                             *
- * You can contact me at dev.jamesvaughan@gmail.com with any questions         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /* FILE PROLOG * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -34,25 +32,9 @@
  * Name       Type           I/O            Description                        *
  * ----       ----           ---            -----------                        *
  *                                                                             *
- * EXTERNAL REFERENCES:                                                        *
- *                                                                             *
- * Name                     Descripton                                         *
- * ----                     ----------                                         *
- *                                                                             *
- * ABNORMAL TERMINATION CONDITIONS, ERROR AND WARNING MESSAGES:                *
- *                                                                             *
  * ASSUMPTIONS, CONSTRAINTS, RESTRICTIONS:                                     *
  *                                                                             *
  * NOTES:                                                                      *
- *                                                                             *
- * REQUIREMENTS/FUNCTIONAL SPECIFICATIONS REFERENCES:                          *
- *                                                                             *
- * DEVELOPMENT HISTORY: (STILL REQUIRED IF USING A VCS)                        *
- *                                                                             *
- * Date       Author    Commit    Release   Description                        *
- * ----       ------    ------    -------   -----------                        *
- *                                                                             *
- * ALGORITHM (PDL)                                                             *
  *                                                                             *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 

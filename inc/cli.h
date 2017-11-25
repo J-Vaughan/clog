3f59c00d1cf8b28d56b7a68f4bec40669272d4d2
James Vaughan <dev.jamesvaughan@gmail.com>
2017-11-24 23:55:49 -0400

    Revert "Shoot for the clouds"
    This reverts commit 76d1a610e9793b875e0274a48f835ec4e7895760.

--- inc/cli.h
+++ inc/cli.h
@@ -1,3 +1,20 @@
+/* HEADER PROLOG * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * NAME:                                                                       *
+ *                                                                             *
+ * PURPOSE:                                                                    *
+ *                                                                             *
+ * GLOBAL VARIABLES:                                                           *
+ *                                                                             *
+ * Variable             Type      Description                                  *
+ * --------             ----      -----------                                  *
+ *                                                                             *
+ * DEVELOPMENT HISTORY: (STILL REQUIRED IF USING A VCS)                        *
+ *                                                                             *
+ * Date       Author    Minor     Release   Description                        *
+ * ----       ------    -----     -------   -----------                        *
+ *                                                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef cli_h
 #define cli_h
 
--- inc/main.h
+++ inc/main.h
@@ -1,3 +1,20 @@
+/* HEADER PROLOG * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * NAME:                                                                       *
+ *                                                                             *
+ * PURPOSE:                                                                    *
+ *                                                                             *
+ * GLOBAL VARIABLES:                                                           *
+ *                                                                             *
+ * Variable             Type      Description                                  *
+ * --------             ----      -----------                                  *
+ *                                                                             *
+ * DEVELOPMENT HISTORY: (STILL REQUIRED IF USING A VCS)                        *
+ *                                                                             *
+ * Date       Author    Minor     Release   Description                        *
+ * ----       ------    -----     -------   -----------                        *
+ *                                                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef main_h
 #define main_h
 
